393e22d2933537b724bc60d9c65df0601c81e3b2
Ajon <ajonsubedi@gmail.com>
2019-06-20 22:04:45 +0200

    contact form created

--- src/app/videos/videos.page.ts
+++ src/app/videos/videos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VideoPlayer } from "@ionic-native/video-player/ngx";
 
 @Component({
   selector: 'app-videos',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class VideosPage implements OnInit {
 
   constructor() { }
+  
 
   ngOnInit() {
   }
